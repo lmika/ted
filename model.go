ea0b8e933ce59ca3071e739cf77614d7a8f4728a
lmika <lmika@localhost>
2015-01-03 10:37:15 +1100

    Rearranged the source files and removed the makefile.  To build run: go build ./...

--- model.go
+++ model.go
@@ -7,10 +7,10 @@ package main
 /**
  * An abstract model interface.
  */
-type IModel interface (
-
+type Model interface {
+    
     /**
      * The dimensions of the model (width, height).
      */
     GetDimensions()     (int, int)
-)
+}
